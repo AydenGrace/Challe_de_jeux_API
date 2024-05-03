b72f545e880b42c9611999a6d43e109b1374fadf
AydenGrace <pa.ce.hepl@gmail.com>
2024-05-03 11:39:18 +0200

    Contact Date

--- models/contact.schema.js
+++ models/contact.schema.js
@@ -5,6 +5,7 @@ const contactSchema = schema({
   email: String,
   subject: { type: String, default: "La Challe de Jeux - Contact" },
   content: String,
+  date: { type: Date, default: Date.now() },
 });
 
 module.exports = contactSchema;
