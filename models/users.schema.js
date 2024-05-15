5b1ace5cb04163b90cf05c97be2edbc0060c51ac
Pierre-Alexandre <pa.ce.hepl@gmail.com>
2024-05-15 14:42:11 +0200

    Test Mailer

--- models/users.schema.js
+++ models/users.schema.js
@@ -1,9 +1,15 @@
 const schema = require("mongoose").Schema;
 
-const userSchema = schema({
-  email: String,
-  password: String,
-  username: { type: String, default: "Guest" },
-});
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    token: String,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = userSchema;
