90379d7c31e8af3d04102a201a42f39a90850f34
Pierre-Alexandre <pa.ce.hepl@gmail.com>
2024-05-15 14:54:42 +0200

    Origin correction

--- models/users.schema.js
+++ models/users.schema.js
@@ -1,6 +1,6 @@
 const schema = require("mongoose").Schema;
 
-const userSchema = new mongoose.Schema(
+const userSchema = new schema(
   {
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
