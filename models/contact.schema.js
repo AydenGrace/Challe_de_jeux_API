37ecfa3e3c2f30e4f50cc3d4a740a8eaf2a114de
AydenGrace <pa.ce.hepl@gmail.com>
2024-05-17 14:18:53 +0200

    Contact mail

--- models/contact.schema.js
+++ models/contact.schema.js
@@ -1,12 +1,16 @@
-const schema = require("mongoose").Schema;
 const mongoose = require("mongoose");
 
-const contactSchema = new schema({
-  name: { type: String, default: "Guest" },
-  email: String,
-  subject: { type: String, default: "La Challe de Jeux - Contact" },
-  content: String,
-  date: { type: Date, default: Date.now() },
-});
+const contactSchema = new mongoose.Schema(
+  {
+    name: { type: String, default: "Guest" },
+    email: String,
+    subject: { type: String, default: "La Challe de Jeux - Contact" },
+    content: String,
+    date: { type: Date, default: Date.now() },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("contact", contactSchema);
--- routes/api/contact.js
+++ routes/api/contact.js
@@ -1,8 +1,5 @@
 const router = require("express").Router();
-const mongoose = require("mongoose");
-const contactSchema = require("./../../models/contact.schema");
 const { getEmailForm } = require("../../contollers/contact-controller");
-const Contact = mongoose.model("contact", contactSchema);
 
 router.get("/", (req, res) => {
   res.send("Contact");
