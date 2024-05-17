41e9204690ec423e49180f1ffd583b31a78fb997
AydenGrace <pa.ce.hepl@gmail.com>
2024-05-17 14:03:38 +0200

    Contact mail

--- models/contact.schema.js
+++ models/contact.schema.js
@@ -1,17 +1,12 @@
 const schema = require("mongoose").Schema;
 const mongoose = require("mongoose");
 
-const contactSchema = new schema(
-  {
-    name: { type: String, default: "Guest" },
-    email: String,
-    subject: { type: String, default: "La Challe de Jeux - Contact" },
-    content: String,
-    date: { type: Date, default: Date.now() },
-  },
-  {
-    timestamps: true,
-  }
-);
+const contactSchema = new schema({
+  name: { type: String, default: "Guest" },
+  email: String,
+  subject: { type: String, default: "La Challe de Jeux - Contact" },
+  content: String,
+  date: { type: Date, default: Date.now() },
+});
 
 module.exports = mongoose.model("contact", contactSchema);
