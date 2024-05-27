69bb3c2cf25f0dfb6a9ef1254d90a24b3462608f
AydenGrace <pa.ce.hepl@gmail.com>
2024-05-27 14:03:56 +0200

    Roles functions (add)

--- contollers/roles-controller.js
+++ contollers/roles-controller.js
@@ -11,4 +11,66 @@ const getAll = async (req, res) => {
     });
 };
 
-module.exports = { getAll };
+const getbyId = async (req, res) => {
+  const { _id } = req.body;
+  Roles.findOne({ _id: _id })
+    .then((roles) => {
+      res.status(200).json(roles);
+    })
+    .catch((e) => {
+      console.error(e);
+      res.status(500).send("Get Error");
+    });
+};
+
+const getbyName = async (req, res) => {
+  const { name } = req.body;
+  Roles.findOne({ name: name })
+    .then((roles) => {
+      res.status(200).json(roles);
+    })
+    .catch((e) => {
+      console.error(e);
+      res.status(500).send("Get Error");
+    });
+};
+
+const getbyPower = async (req, res) => {
+  const { power } = req.body;
+  Roles.findOne({ power: power })
+    .then((roles) => {
+      res.status(200).json(roles);
+    })
+    .catch((e) => {
+      console.error(e);
+      res.status(500).send("Get Error");
+    });
+};
+
+const addOne = async (req, res) => {
+  const { name, power } = req.body;
+  const isAlreadyExist = await Roles.findOne({
+    $or: [{ name: name }, { power: power }],
+  });
+  console.log(isAlreadyExist);
+  if (isAlreadyExist) {
+    res.status(400).json({ message: "Role already exist." });
+    return;
+  }
+  const newRole = new Roles({
+    name,
+    power,
+  });
+  await newRole
+    .save()
+    .then((t) => {
+      console.log(t);
+      res.status(200).json(t);
+    })
+    .catch((e) => {
+      console.error(e);
+      res.status(400).send("Add Error");
+    });
+};
+
+module.exports = { getAll, getbyId, getbyName, getbyPower, addOne };
--- routes/api/roles.js
+++ routes/api/roles.js
@@ -1,7 +1,21 @@
-const { getAll } = require("../../contollers/roles-controller");
+const {
+  getAll,
+  getbyId,
+  getbyName,
+  getbyPower,
+  addOne,
+} = require("../../contollers/roles-controller");
 
 const router = require("express").Router();
 
 router.get("/", getAll);
+
+router.get("/id", getbyId);
+
+router.get("/name", getbyName);
+
+router.get("/power", getbyPower);
+
+router.post("/add", addOne);
 
 module.exports = router;
