83379a098a12c4330db85e220b757072ce07d027
AydenGrace <pa.ce.hepl@gmail.com>
2024-05-27 08:56:44 +0200

    Populate preparation

--- contollers/cats-controller.js
+++ contollers/cats-controller.js
@@ -1,7 +1,7 @@
 const Cats = require("../models/cats.schema");
 
-const getCats = async (req,res) => {
-    Cats.find()
+const getCats = async (req, res) => {
+  Cats.find()
     .then((cats) => {
       res.status(200).json(cats);
     })
@@ -9,32 +9,43 @@ const getCats = async (req,res) => {
       console.error(e);
       res.status(500).send("Get Error");
     });
-}
+};
 
-const addCat = async (req,res) => {
-    const body = req.body;
-    console.log(body);
-    const newCat = new Cats(body);
-    newCat
-        .save()
-        .then((t) => {
-        console.log(t);
-        res.status(200).json(t);
-        })
-        .catch((e) => {
-        console.error(e);
-        res.status(500).send("Add Error");
-        });
-}
+const getFullCats = async (req, res) => {
+  Cats.find()
+    .then((cats) => {
+      res.status(200).json(cats);
+    })
+    .catch((e) => {
+      console.error(e);
+      res.status(500).send("Get Error");
+    });
+};
 
-const deleteCat = async (req,res) => {
-    console.log(req);
-    res.send("Chat Supprimé");
-}
+const addCat = async (req, res) => {
+  const body = req.body;
+  console.log(body);
+  const newCat = new Cats(body);
+  newCat
+    .save()
+    .then((t) => {
+      console.log(t);
+      res.status(200).json(t);
+    })
+    .catch((e) => {
+      console.error(e);
+      res.status(500).send("Add Error");
+    });
+};
 
-const updateCat = async (req,res) => {
-    console.log(req);
-    res.send("Chat Modifié");
-}
+const deleteCat = async (req, res) => {
+  console.log(req);
+  res.send("Chat Supprimé");
+};
 
-module.exports = {getCats,addCat,deleteCat,updateCat}
+const updateCat = async (req, res) => {
+  console.log(req);
+  res.send("Chat Modifié");
+};
+
+module.exports = { getCats, addCat, deleteCat, updateCat, getFullCats };
--- routes/api/cats.js
+++ routes/api/cats.js
@@ -1,9 +1,15 @@
-const { getCats, addCat, deleteCat, updateCat } = require("../../contollers/cats-controller");
+const {
+  getCats,
+  addCat,
+  deleteCat,
+  updateCat,
+  getFullCats,
+} = require("../../contollers/cats-controller");
 
 const router = require("express").Router();
 
-
 router.get("/", getCats);
+router.get("/getFull", getFullCats);
 
 router.post("/add", addCat);
 
