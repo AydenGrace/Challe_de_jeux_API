8474ff52b8a35d5117c652c1913635e502c08301
AydenGrace <pa.ce.hepl@gmail.com>
2024-07-12 15:37:43 +0200

    Dev or deploy switch

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ console.log(allowedOrigin);
 app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", allowedOrigin);
+  res.header("Access-Control-Allow-Origin", process.env.ALLOWED_URL);
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Access-Control-Allow-Credentials", "true");
